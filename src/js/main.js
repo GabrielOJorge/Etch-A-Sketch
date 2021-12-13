9b5a5bf4b9f9f35313ccab75908cc5e256735480
GabrielOJorge <gabrieloliveiraj331@gmail.com>
2021-12-13 13:39:25 -0300

    Delete grid-btn and add a slider that defines the sketch size

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,25 @@
 const mainContainer = document.querySelector("#container");
 const colorBtn = document.querySelector("#color-btn");
+const randomColorBtn = document.querySelector("#random-color-btn");
 const rainbowBtn = document.querySelector("#rainbow-btn");
 const clearBtn = document.querySelector("#clear-btn");
-const gridBtn = document.querySelector("#grid-btn");
+const sizeValue = document.querySelectorAll(".size-value");
+const sliderSize = document.querySelector("#slider-size");
 
-let size = 16;
+let size = sliderSize.value;
+let color = '#000';
 
-const setGridSize = (color = "#000") => {
+for (let i = 0; i < sizeValue.length; i++) {
+  sizeValue[i].textContent = `${size}`;
+}
+
+const setGridSize = () => {
   mainContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   mainContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
+  
   for (let i = 0; i <= size * size; i++) {
     let divs = document.createElement("div");
-    divs.addEventListener("mouseover", () => divs.style.background = color);
+    divs.addEventListener("mouseover", () => divs.style.background = `${color}`);
     mainContainer.appendChild(divs);
   }
 };
@@ -23,29 +30,32 @@ const clearGrid = function() {
   mainContainer.innerHTML = ''
 };
 
-gridBtn.addEventListener("click", () => {
+const getRandomNumber = (limit) => {
+  return Math.floor(Math.random() * limit);
+};
+
+const getRandomColor = () => {
+  const h = getRandomNumber(360);
+  const s = getRandomNumber(100);
+  const l = getRandomNumber(100);
+
+  return `hsl(${h}deg, ${s}%, ${l}%)`;
+};
+
+sliderSize.oninput = (() => {
+  size = sliderSize.value;
+  
+  for (let i = 0; i < sizeValue.length; i++) {
+    sizeValue[i].textContent = `${size}`;
+  }
+
   clearGrid();
-  size = prompt("Size:");
   setGridSize();
 });
 
-rainbowBtn.addEventListener("click", () => {
-  const getRandomNumber = (limit) => {
-    return Math.floor(Math.random() * limit);
-  };
-
-  const getRandomColor = () => {
-    const h = getRandomNumber(360);
-    const s = getRandomNumber(100);
-    const l = getRandomNumber(100);
-
-    return `hsl(${h}deg, ${s}%, ${l}%)`;
-  };
-
+randomColorBtn.addEventListener("click", () => {
   const randomColor = getRandomColor();
-
-  setGridSize(16, randomColor);
-  divs.addEventListener("mouseover", () => divs.style.background = randomColor);
+  color = randomColor;
 });
 
 clearBtn.addEventListener("click", () => {
