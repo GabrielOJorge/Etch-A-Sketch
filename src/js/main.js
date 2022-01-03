9e5428cacfa5a68f59215fb4d59e0a7a908131da
GabrielOJorge <gabrieloliveiraj331@gmail.com>
2022-01-03 17:52:55 -0300

    refactor: change 'container' id name to 'sketch' and change clearGrid function its way of working

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const mainContainer = document.querySelector("#container");
+const sketch = document.querySelector("#sketch");
 const randomColorBtn = document.querySelector("#random-color-btn");
 const rainbowBtn = document.querySelector("#rainbow-btn");
 const clearBtn = document.querySelector("#clear-btn");
@@ -14,32 +14,34 @@ sizeValue.forEach(value => {
 });
 
 const setGrid = () => {
-  mainContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  mainContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  sketch.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  sketch.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   
-  for (let i = 0; i <= size * size; i++) {
+  for (let i = 0; i < size * size; i++) {
     let divs = document.createElement("div");
     divs.addEventListener("mouseover", () => divs.style.background = color)
-    mainContainer.appendChild(divs);
+    sketch.appendChild(divs);
   }
 };
 
 const setRainbowGrid = () => {
-  mainContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  mainContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  sketch.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  sketch.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   
   for (let i = 0; i <= size * size; i++) {
     const randomColor = getRandomColor();
     let divs = document.createElement("div");
     divs.addEventListener("mouseover", () => divs.style.background = randomColor);
-    mainContainer.appendChild(divs);
+    sketch.appendChild(divs);
   }
 };
 
 setGrid();
 
-const clearGrid = function() {
-  mainContainer.innerHTML = ''
+const clearGrid = () => {
+  while (sketch.lastChild) {
+    sketch.removeChild(sketch.lastChild);
+  }
 };
 
 const getRandomNumber = (limit) => {
@@ -57,9 +59,9 @@ const getRandomColor = () => {
 sliderSize.oninput = (() => {
   size = sliderSize.value;
   
-  for (let i = 0; i < sizeValue.length; i++) {
-    sizeValue[i].textContent = size;
-  }
+  sizeValue.forEach(value => {
+    value.textContent = size;
+  });
 
   clearGrid();
   setGrid();
